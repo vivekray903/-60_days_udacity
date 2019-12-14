9451b931523699b398c09ad790773a62e1f3e850
Vivek Ray <41966135+vivekray903@users.noreply.github.com>
2019-12-14 23:24:31 +0530

    Create Day5.sh

---
+++ Day5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Program to install basic software on linux
+sudo -s
+apt-get update
+apt-get  install vlc 
+apt-get install chromium-browser
+apt-get install playonlinux
+apt-get install slack
